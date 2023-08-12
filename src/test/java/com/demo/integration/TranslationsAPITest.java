24ab12ff365d9d8d99ad325d300f6c165ca589c2
Robert Heim <robert@robert-heim.de>
2023-08-12 17:03:24 +0200

    organize imports

--- src/test/java/com/demo/integration/TranslationsAPITest.java
+++ src/test/java/com/demo/integration/TranslationsAPITest.java
@@ -1,13 +1,7 @@
 package com.demo.integration;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 
 import org.flywaydb.core.Flyway;
@@ -18,24 +12,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.demo.questionnaire.internal.model.questiontypes.MultipleChoiceConfig;
-import com.demo.questionnaire.internal.model.questiontypes.Option;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.demo.i18n.model.Language;
 import com.demo.i18n.model.LanguageRepository;
-import com.demo.i18n.model.TranslationsDTO;
 import com.demo.questionnaire.internal.model.Question;
 import com.demo.questionnaire.internal.model.QuestionI18n;
 import com.demo.questionnaire.internal.model.QuestionI18n.QuestionI18nId;
 import com.demo.questionnaire.internal.model.QuestionI18nRepository;
 import com.demo.questionnaire.internal.model.QuestionRepository;
+import com.demo.questionnaire.internal.model.questiontypes.MultipleChoiceConfig;
+import com.demo.questionnaire.internal.model.questiontypes.Option;
 
 import jakarta.persistence.EntityManager;
 import lombok.extern.slf4j.Slf4j;
